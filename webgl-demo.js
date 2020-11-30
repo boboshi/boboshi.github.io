c0bf36ca2eff979dae8b1558ab57d2f0c893a051
mup <muffinboyz008@hotmail.com>
2020-11-30 15:20:02 +0800

    hello world done for real

--- webgl-demo.js
+++ webgl-demo.js
@@ -3,4 +3,20 @@ main();
 function main()
 {
 	document.write("Hello World!");
+	
+	// select canvas and get gl context
+	const canvas = document.querySelector("#glCanvas");
+	const gl = canvas.getContext("webgl");
+	
+	// check if webgl init successfully
+	if (gl === null)
+	{
+		alert("webgl gg bro");
+		return;
+	}
+	
+	// set global clear colour to black
+	gl.clearColor(0.0, 0.0, 0.0, 1.0);
+	// clearrrrr
+	gl.clear(gl.COLOR_BUFFER_BIT);
 }
