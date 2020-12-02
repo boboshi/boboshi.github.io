7b33228d2e2ca3385bdbebc54c2b40b0fccdfd07
mup <muffinboyz008@hotmail.com>
2020-12-02 11:02:48 +0800

    loading fbx works somewhat??

--- threejs-demo.js
+++ threejs-demo.js
@@ -1,6 +1,7 @@
 // imports
-import * as THREE from "http://10.1.11.197:8080/node_modules/three/build/three.module.js";
-import {OrbitControls} from "http://10.1.11.197:8080/node_modules/three/examples/jsm/controls/OrbitControls.js"
+import * as THREE from "../node_modules/three/build/three.module.js";
+import {OrbitControls} from "../node_modules/three/examples/jsm/controls/OrbitControls.js"
+import {FBXLoader} from "../node_modules/three/examples/jsm/loaders/FBXLoader.js"
 
 // init scene, camera and renderer
 const scene = new THREE.Scene();
@@ -15,31 +16,14 @@ camera.position.z = 10;
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-// modify three.js' orbitcontrols to be more intuitive
-//THREE.MapControls = function (object, domElement) {
-//
-//	THREE.OrbitControls.call( this, object, domElement );
-//
-//	this.mouseButtons.LEFT = THREE.MOUSE.PAN;
-//	this.mouseButtons.RIGHT = THREE.MOUSE.ROTATE;
-//
-//	this.touches.ONE = THREE.TOUCH.PAN;
-//	this.touches.TWO = THREE.TOUCH.DOLLY_ROTATE;
-//};
-//
-//THREE.MapControls.prototype = Object.create( THREE.EventDispatcher.prototype );
-//THREE.MapControls.prototype.constructor = THREE.MapControls;
-//
-//// camera controls
-//const controls = new THREE.MapControls(camera, renderer.domElement);
-//
-//controls.enableDamping = true;
-//controls.dampingFactor = 0.05;
-//controls.screenSpacePanning = false;
-//
-//controls.maxPolarAngle = Math.PI / 2;
-
+// camera controls
 const controls = new OrbitControls(camera, renderer.domElement);
+
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
+controls.screenSpacePanning = false;
+
+controls.maxPolarAngle = Math.PI / 2;
 
 // add basic green cube to scene
 const boxgeometry = new THREE.BoxGeometry();
@@ -57,36 +41,21 @@ const transparentmaterial = new THREE.MeshPhongMaterial(
 	side: THREE.DoubleSide,
 });
 
-// GLTF model loader
-//const loader = new THREE.GLTFLoader();
-//loader.load
-//(
-//	"http://10.1.11.197:8080/resources/ResidentialBuildings001.gltf", function (GLTF) 
-//		{
-//			scene.add(GLTF.scene);
-//		}, 
-//		undefined, 
-//		function (error ) 
-//		{
-//			console.error(error);
-//		} 
-//);
+// fbx model loader
+const loader = new FBXLoader();
 
-// fbx model loader (not working for now)
-//const loader = new THREE.FBXLoader();
-//
-//loader.load
-//(
-//	"http://10.1.11.197:8080/resources/room.fbx", function (fbx) 
-//		{
-//			scene.add(fbx.scene);
-//		}, 
-//		undefined, 
-//		function (error ) 
-//		{
-//			console.error(error);
-//		} 
-//);
+loader.load
+(
+	"http://10.1.11.197:8080/resources/room.fbx", function (fbx) 
+		{
+			scene.add(fbx);
+		}, 
+		undefined, 
+		function (error ) 
+		{
+			console.error(error);
+		}
+);
 
 // draw grid
 const size = 100;
