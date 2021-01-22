424a2d051cd6fa4055952c83c1733f9267ec642c
mup <muffinboyz008@hotmail.com>
2021-01-22 18:21:31 +0800

    progress boys

--- react/lms/src/Login.js
+++ react/lms/src/Login.js
@@ -1,18 +1,17 @@
 import React, {useState} from "react";
 import LoginForm from "./components/LoginForm";
 import BG from "./resources/login/nastuh-abootalebi-284883-unsplash.png";
-import Copyright from "./resources/login/copyright.png"
-import PPTC from "./resources/login/Privacy Policy · Terms & Conditions.png"
 
 function Login(props)
 {
     return (
         <div className = "login-page">
             <img src = {BG} className = "login-page-bg"></img>
-            <div className = "login-page-footer"></div>
-            <img src = {Copyright} className = "login-page-footer-copyright"></img>
-            <img src = {PPTC} className = "login-page-footer-pptc"></img>
             <LoginForm />
+            <div className = "login-page-footer">
+                <h1 className = "login-page-footer-copyright">COPYRIGHT © 2020 AZTECH TECHNOLOGIES PTE LTD. ALL RIGHTS RESERVED.</h1>
+                <h2 className = "login-page-footer-privacy">PRIVACY POLICY · TERMS & CONDITIONS</h2>
+            </div>
         </div>
     );
 }
--- react/lms/src/components/LoginForm.js
+++ react/lms/src/components/LoginForm.js
@@ -1,15 +1,24 @@
 import React, {useState} from "react";
 import LoginRect from "../resources/login/Rectangle 2.png"
+import Line from "../resources/login/Path 1.png"
+import Logo from "../resources/login/Aztech logo 2020-WH.png"
 
 function LoginForm(props)
 {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
-    function handleSubmit(e)
+    function handleSubmitNext(e)
     {
         e.preventDefault();
-        console.log("submitted");
+        console.log({email});
+        console.log({password});
+    }
+
+    function handleForgotPW(e)
+    {
+        e.preventDefault();
+        console.log("forgot pw");
     }
 
     function handleChangeEmail(e)
@@ -23,14 +32,18 @@ function LoginForm(props)
     }
 
     return (
-        <div className = "login-form">
-            <img src = {LoginRect} className = "login-form-rect"></img>
-            <form onSubmit = {handleSubmit}>
+        <div className = "login-page-form">
+            <img src = {LoginRect} className = "login-page-form-rect"></img>
+            <h1 className = "login-page-form-welcome">Welcome Back!</h1>
+            <img src = {Logo} className = "login-page-form-logo"></img>
+            <h2 className = "login-page-form-logintext">LOGIN</h2>
+            <h3 className = "login-page-form-lmstext">Lighting Management System (LMS)</h3>
+            <form onSubmit = {handleSubmitNext}>
                 <div>
                     <input
                         type = "text"
                         id = "login-email"
-                        className = "login-input-email"
+                        className = "login-page-form-email"
                         name = "email"
                         value = {email}
                         placeholder = "EMAIL"
@@ -39,9 +52,9 @@ function LoginForm(props)
                 </div>
                 <div>
                     <input
-                        type = "text"
+                        type = "password"
                         id = "login-password"
-                        className = "login-input-password"
+                        className = "login-page-form-password"
                         name = "password"
                         value = {password}
                         placeholder = "PASSWORD"
@@ -49,11 +62,11 @@ function LoginForm(props)
                     ></input>
                 </div>
                 <div>
-                    <button type = "submit" className = "login-btn">
-                        Next
-                    </button>
+                    <button type = "submit" className = "login-page-form-nextbtn"></button>
                 </div>
-
+            </form>
+            <form onSubmit = {handleForgotPW}>
+                <button type = "submit" className = "login-page-form-pwbtn"></button>
             </form>
         </div>
     );
